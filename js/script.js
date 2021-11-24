3812f9d2b456c236be2c85bfb536bfecfcafe97d
ztarinc <stevencrosario@gmail.com>
2021-11-23 19:38:13 -0500

    " Diseño Web (Practicas)

---
+++ js/script.js
@@ -0,0 +1,70 @@
+//Cajas Selecionables con NEUMORFISMO
+
+document.querySelector(".box1").addEventListener("click", box_selected1);
+document.querySelector(".box2").addEventListener("click", box_selected2);
+document.querySelector(".box3").addEventListener("click", box_selected3);
+
+//Declaración de variables
+
+//box1
+
+box1 = document.querySelector(".box1");
+check_bg1 = document.querySelector(".box1 .check");
+icon_check1 = document.querySelector(".box1 .fa-check");
+
+//box2
+
+box2 = document.querySelector(".box2");
+check_bg2 = document.querySelector(".box2 .check");
+icon_check2 = document.querySelector(".box2 .fa-check");
+
+//box3
+
+box3 = document.querySelector(".box3");
+check_bg3 = document.querySelector(".box3 .check");
+icon_check3 = document.querySelector(".box3 .fa-check");
+
+
+function box_selected1 () {
+		box2.classList.remove('box-selected');
+		check_bg2.classList.remove('check-selected');
+		icon_check2.classList.remove('icon-check');
+
+		box3.classList.remove('box-selected');
+		check_bg3.classList.remove('check-selected');
+		icon_check3.classList.remove('icon-check');
+
+		box1.classList.toggle('box-selected');
+		check_bg1.classList.toggle('check-selected');
+		icon_check1.classList.toggle('icon-check');
+}
+
+function box_selected2 () {
+		box2.classList.toggle('box-selected');
+		check_bg2.classList.toggle('check-selected');
+		icon_check2.classList.toggle('icon-check');
+
+		box1.classList.remove('box-selected');
+		check_bg1.classList.remove('check-selected');
+		icon_check1.classList.remove('icon-check');
+
+		box3.classList.remove('box-selected');
+		check_bg3.classList.remove('check-selected');
+		icon_check3.classList.remove('icon-check');
+}
+
+function box_selected3 () {
+		box3.classList.toggle('box-selected');
+		check_bg3.classList.toggle('check-selected');
+		icon_check3.classList.toggle('icon-check');
+
+		box1.classList.remove('box-selected');
+		check_bg1.classList.remove('check-selected');
+		icon_check1.classList.remove('icon-check');
+
+		box2.classList.remove('box-selected');
+		check_bg2.classList.remove('check-selected');
+		icon_check2.classList.remove('icon-check');
+}
+
+
